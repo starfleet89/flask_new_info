760093d1e2757d90ddbb04e25c8d1887795749f4
zhaoXiY <45872317+zhaoXiY@users.noreply.github.com>
2020-06-18 20:21:42 +0800

    网页图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blu
 @index_blu.route('/')
 def index():
     #return "index"
     return render_template('news/index.html')
+@index_blu.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
