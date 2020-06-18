accd46b05e535b674521c0b9e2d995926d0f8123
zhaoXiY <45872317+zhaoXiY@users.noreply.github.com>
2020-06-18 18:54:43 +0800

    首页页面

--- info/__init__.py
+++ info/__init__.py
@@ -28,6 +28,9 @@ def create_app(config_name):
     CSRFProtect(app)
     # 设置session保存指定位置
     Session(app)
+    #注册蓝图时，导入和注册写在一起
+    from info.modules.index import index_blu
+    app.register_blueprint(index_blu)
     return app
 #记录日志
 def log_file(level):
