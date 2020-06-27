900c075e28e58bd2c20d8619767ea0370445c45c
zhaoXiY <45872317+zhaoXiY@users.noreply.github.com>
2020-06-27 23:37:12 +0800

    点击排行

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, current_app, session
 
 from . import index_blu
-from ...models import User
+from ... import constants
+from ...models import User, News
 
 
 @index_blu.route('/')
@@ -12,13 +13,24 @@ def index():
     user_id = session.get("user_id")
     #通过id查询用户信息
     user = None
+    news_list = None
     if user_id:
         try:
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
-
-    return render_template('news/index.html',data={"user_info":user.to_dict() if user else None})
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e :
+        current_app.logger.error(e)
+    click_news_list = []
+    for news in news_list if news_list else []:
+        click_news_list.append(news.to_basic_dict())
+    data = {
+        "user_info":user.to_dict() if user else None,
+        "click_news_list":click_news_list,
+    }
+    return render_template('news/index.html',data=data)
   # return "dada"
 #浏览器在访问,在访问每个网站的时候,都会发送一个Get请求,向/favicon.ico地址获取logo
 #app中提供了方法send_static_file,会自动寻找static静态文件下面的资源
